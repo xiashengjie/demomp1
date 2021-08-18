d8e3c1408208fa6c9fbbe420c56f6f49ca6ed834
xiashengjie <xiasj@uniplore.io>
2021-08-19 00:13:12 +0800

    提交

--- src/test/java/com/xsj/demomp1/Demomp1ApplicationTests.java
+++ src/test/java/com/xsj/demomp1/Demomp1ApplicationTests.java
@@ -20,4 +20,22 @@ class Demomp1ApplicationTests {
 
     }
 
+    @Test
+    public void insertUser() {
+        User user = new User();
+        user.setName("xsj");
+        user.setAge(22);
+        user.setEmail("372192060@qq.com");
+
+        int insert = userMapper.insert(user);
+    }
+
+    @Test
+    public void updateUser() {
+        User user = new User();
+        user.setId(10L);
+        user.setAge(20);
+
+        int insert = userMapper.updateById(user);
+    }
 }

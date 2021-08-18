39ab301c007a789cabc48cefc7dec8d626959946
xiashengjie <xiasj@uniplore.io>
2021-08-18 18:10:53 +0800

    提交

---
+++ src/main/java/com/xsj/demomp1/Demomp1Application.java
@@ -0,0 +1,13 @@
+package com.xsj.demomp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demomp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demomp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xsj/demomp1/Demomp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsj.demomp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demomp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
